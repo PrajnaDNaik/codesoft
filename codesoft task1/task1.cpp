e5ab353eb727219850b80d8648124a7ed7163485
PrajnaDNaik <163340714+PrajnaDNaik@users.noreply.github.com>
2026-04-23 19:23:11 +0530

    Add files via upload
    This is a simple C++ console-based game where the program generates a random number within a given range (e.g., 1 to 100). The player is asked to guess the number, and after each attempt, the program provides feedback indicating whether the guess is too high or too low. The game continues until the correct number is guessed. It also tracks the number of attempts taken by the user.
    
    This project demonstrates basic programming concepts such as loops, conditionals, user input, and random number generation.

---
+++ codesoft task1/task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+    srand(time(0));
+    int number = rand() % 100 + 1; 
+    int guess;
+    cout << "Number Guessing Game\n";
+    cout << "Guess a number between 1 and 100:\n";
+    while (true) {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        if (guess > number) {
+            cout << "Too high Try again.\n";
+        } else if (guess < number) {
+            cout << "Too low Try again.\n";
+        } else {
+            cout << "Correct You guessed the number\n";
+            break;
+        }
+    }
+    return 0;
+}
